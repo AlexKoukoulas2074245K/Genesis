53e912e4219ea1002d94c4da3ffdb2c0d960d7bb
Alex <alex.koukoulas@king.com>
2019-12-29 22:59:24 +0100

    Added lua for macosx

--- source/game/Main.cpp
+++ source/game/Main.cpp
@@ -8,10 +8,20 @@
 #include "Game.h"
 #include "../engine/GenesisEngine.h"
 
+extern "C"{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+
 ///------------------------------------------------------------------------------------------------
 
 int main(int, char**)
 {
+    lua_State *L = luaL_newstate(); /* opens Lua */
+    luaL_openlibs(L);
+    
     genesis::GenesisEngine engine;
     genesis::GameStartupParameters startupParameters("Genesis", 0.3f);
 
